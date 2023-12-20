45b275f813a3e84fe79fd1c567f3f1de5f198e5f
yanxu <yanxu.wei@mediatek.com>
2023-12-20 15:12:58 +0800

    fs_mgr: Add exfat fs in fs_mgr
    Need to support exfat fs type point mount in recovery mode
    
    Bug:312674538
    Test: SIU (Apply update from SD card) in recovery mode
    
    Change-Id: I2735e5068c47b183a25d2305bca89f08de1e32c7

--- fs_mgr/fs_mgr_roots.cpp
+++ fs_mgr/fs_mgr_roots.cpp
@@ -115,7 +115,7 @@ bool TryPathMount(FstabEntry* rec, const std::string& mount_pt) {
         return true;
     }
 
-    static const std::vector<std::string> supported_fs{"ext4", "squashfs", "vfat", "f2fs", "erofs",
+    static const std::vector<std::string> supported_fs{"ext4", "squashfs", "vfat", "exfat", "f2fs", "erofs",
                                                        "none"};
     if (std::find(supported_fs.begin(), supported_fs.end(), rec->fs_type) == supported_fs.end()) {
         LERROR << "unknown fs_type \"" << rec->fs_type << "\" for " << mount_point;
